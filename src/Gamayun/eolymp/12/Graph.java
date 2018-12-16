ff71fa2b4cc9ed819ba21897e3ad7052ddd79355
Anna <a.karlysheva@ukma.edu.ua>
2018-12-16 16:04:03 +0200

    Гамаюн 12-14

---
+++ src/Gamayun/eolymp/12/Graph.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Graph {
+    static class OneRow {
+        private int count;
+        private int[] ver;
+
+        OneRow(int count, int[] ver) {
+            this.count = count;
+            this.ver = ver;
+        }
+    }
+
+    public static void main(String[] args) {
+        BufferedReader bf;
+        PrintWriter out = new PrintWriter(System.out);
+        try {
+            bf = new BufferedReader(new InputStreamReader(System.in));
+            int vertexes = Integer.parseInt(bf.readLine());
+            OneRow[] ar = new OneRow[vertexes];
+            for (int j = 0; j < ar.length; j++) {
+                ar[j] = new OneRow(0, new int[1]);
+            }
+            String line;
+
+            for (int i = 0; i < vertexes; i++) {
+                if ((line = bf.readLine()).equals("")) {
+                    continue;
+                }
+                StringTokenizer str = new StringTokenizer(line, " ");
+                int k;
+                int length;
+                while (str.hasMoreTokens()) {
+                    k = (Integer.parseInt(str.nextToken()) - 1);
+                    length = ar[k].ver.length;
+                    //resize array
+                    if (ar[k].count == length) {
+                        int[] temp = new int[length * 2];
+                        for (int j = 0; j < length; j++) {
+                            temp[j] = ar[k].ver[j];
+                        }
+                        ar[k].ver = temp;
+                    }
+
+                    ar[k].ver[ar[k].count++] = i + 1;
+
+                }
+            }
+
+            out.println(vertexes);
+            for (int i = 0; i < ar.length; i++) {
+                for (int j = 0; j < ar[i].count; j++) {
+                    if (ar[i].count != 0) {
+
+                        out.print(ar[i].ver[j] + " ");
+
+                    }
+                }
+                out.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        out.flush();
+    }
+}
---
+++ src/Gamayun/eolymp/13/Unition.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Unition {
+
+	static int i, j, n, v, to;
+	
+	static int[] x = new int[5001];
+	static int[] y = new int[5001];
+
+	static int[] used = new int[5001];
+	static int[] min_e = new int[5001];
+	static int[] end_e = new int[5001];
+	
+	static int sqr(int x) { return x*x; }
+
+	static int mesafe(int i, int j) { return sqr(x[j] - x[i]) + sqr(y[j] - y[i]); }
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		
+		for(int i = 0;i < n;i++)
+		{
+			x[i] = sc.nextInt();
+			y[i] = sc.nextInt();	
+		}
+		
+		Arrays.fill(min_e, 0x3F);
+		Arrays.fill(end_e, -1);
+		Arrays.fill(used, 0);
+		double dist = min_e[1] = 0;
+		
+		
+		for (i = 0; i < n; i++)
+		{
+			v = -1;
+			for (j = 0; j < n; j++)
+				if (used[j] == 0 && (v == -1 || min_e[j] < min_e[v])) v = j;
+
+			used[v] = 1;
+			if (end_e[v] != -1) dist += Math.sqrt(mesafe(v, end_e[v]));
+
+			for (to = 0; to < n; to++)
+			{
+				int dV_TO = mesafe(v, to);
+				if (used[to] ==0 && dV_TO < min_e[to])
+				{
+					min_e[to] = dV_TO;
+					end_e[to] = v;
+				}
+			}
+		}
+		
+		System.out.println(dist);
+	}
+
+}
